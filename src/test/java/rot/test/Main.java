f37e4591eebcf904794af0fb3302acd6b3e0c934
SWSpratlin <120224974+SWSpratlin@users.noreply.github.com>
2024-09-12 09:37:14 -0400

    changing cam reaction

--- src/test/java/rot/test/Main.java
+++ src/test/java/rot/test/Main.java
@@ -73,7 +73,7 @@ public class Main extends PApplet {
         //Processed Video Stream
         for (int i = 0; i < max; i++) {
             if (Utils.osName.contains("linux")) {
-                if (temp[i] > -135000) {
+                if (temp[i] > -10000) {
                     a.pixels[i] = 0x00000000;
                 }
             } else if (Utils.osName.contains("mac")) {
